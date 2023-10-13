61997d25c1309b3e4d5276b2b70cb326abb93da3
AntonyRevda <147719308+AntonyRevda@users.noreply.github.com>
2023-10-13 12:41:56 +0300

    Add files via upload

---
+++ Лабораторная работа 1/task_3.py
@@ -0,0 +1,10 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+count = len(list_players)
+# team_1 = list_players[0::2]
+# team_2 = list_players[1::2]
+team_1 = list_players[0:3]
+team_2 = list_players[3:]
+print(team_1)
+print(team_2)
