5ebc32dbf9088f5baab3d96ec295f0561a9d1079
AntonyRevda <147719308+AntonyRevda@users.noreply.github.com>
2023-10-29 21:33:11 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task_1.py
@@ -0,0 +1,16 @@
+# TODO Напишите функцию для поиска индекса товара
+def index_search(list_, item):
+    if item in list_:
+        index = list_.index(item)
+        return index
+    else:
+        return None
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = index_search(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
